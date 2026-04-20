b75fb382aee1318d6338c6e21f0e056a09e944d3
Sidharth P Pai <sidharthppai@gmail.com>
2026-04-20 14:25:07 +0530

    Add files via upload

---
+++ Arrays/arrsd.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float agr_sum(float* parr,int count){
+
+	float sum = 0;
+
+	for (int i = 0 ; i < count; i++){
+
+		sum = sum + parr[i];
+	
+	}
+
+	return sum;
+}
+
+float average(float* parr,int count){
+
+	float sum = agr_sum(parr,count);
+
+	float avg = sum / count;
+
+	return avg;
+
+}
+
+float std_dev(float* parr,int count){
+
+	float av = average(parr,count);
+
+	float sum = 0;
+
+	for(int i = 0 ; i < count ; i++){
+
+		sum = sum + ((parr[i]-av)*(parr[i]-av));
+
+	}
+
+	return sqrt(sum/count);
+
+}
+
+
+int main(int argc,char **argv){
+
+	if (argc < 2){
+
+		printf("Command line error");
+		return -1;
+	}
+
+	float* parr = (float*)malloc((argc-1)*sizeof(float));
+
+
+	if(parr==NULL){
+
+
+		printf("Error in accessing array");
+		return -1;
+
+	}
+
+	int count = argc - 1;
+
+	for (int i = 1; i <= count ; i++){
+
+		parr[i-1] = atof(argv[i]); 
+	
+
+	}
+
+	float result = agr_sum(parr,count);
+	float avg = average(parr,count);
+	float st = std_dev(parr,count);
+
+	printf("The sum of the dynamic array is %.2f\n",result);
+	printf("Average of the array is %.2f\n",avg);
+	printf("The standard deviation of the array is %.2f",st);	
+
+	free(parr);
+
+	return 0;
+}
